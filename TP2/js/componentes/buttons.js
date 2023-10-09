3bd162fd07d60ffcc32a7a4cc1d7afd79b96a693
Nacho <nachogassie@gmail.com>
2023-10-09 05:29:51 -0300

    juegos agregados al carrusel

--- TP2/js/componentes/buttons.js
+++ TP2/js/componentes/buttons.js
@@ -1,7 +1,7 @@
 function crearYaCuentaBtn(){
     const yaCuentaBtn = document.createElement("button");
     yaCuentaBtn.classList.add("login_cambiar-form_btn");
-    yaCuentaBtn.classList.add("button-Terciary");
+    yaCuentaBtn.classList.add("button-terciary");
     yaCuentaBtn.setAttribute("id", "ya-cuenta-btn");
     const spanElement = document.createElement("span");
     spanElement.textContent = "¿Ya tienes cuenta?";
